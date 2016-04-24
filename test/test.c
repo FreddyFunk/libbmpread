f819c0bc86f77e0945e7f927d7ff6448047022d8
Charles Lindsay <chaz@chazomatic.us>
2016-04-24 12:47:04 -0700

    Add 2016 copyright
    I've worked on it this year, so it gets called out in the copyright
    legalese.

--- test/test.c
+++ test/test.c
@@ -1,7 +1,7 @@
 /******************************************************************************
 * libbmpread - tiny, fast bitmap (.bmp) image file loader                     *
 *              <https://github.com/chazomaticus/libbmpread>                   *
-* Copyright (C) 2005, 2012 Charles Lindsay <chaz@chazomatic.us>               *
+* Copyright (C) 2005, 2012, 2016 Charles Lindsay <chaz@chazomatic.us>         *
 *                                                                             *
 *  This software is provided 'as-is', without any express or implied          *
 *  warranty.  In no event will the authors be held liable for any damages     *
@@ -43,7 +43,7 @@ int main(int argc, char * argv[])
     bmpread_t bmp;
 
     puts("Test utility for libbmpread");
-    puts("Copyright (C) 2005, 2012 Charles Lindsay <chaz@chazomatic.us>");
+    puts("Copyright (C) 2005, 2012, 2016 Charles Lindsay <chaz@chazomatic.us>");
     puts("");
 
     if(argc < 2)
