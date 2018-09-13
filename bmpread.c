d7479863b73ec6ef37e4a6891c22f7c7f509df20
Frederic L <27208977+FreddyFunk@users.noreply.github.com>
2018-09-13 04:45:27 +0200

    Fixed code format

--- bmpread.c
+++ bmpread.c
@@ -917,13 +917,13 @@ int bmpread(const char * bmp_file, unsigned int flags, bmpread_t * p_bmp_out)
         ctx.flags = flags;
 
         /* Use "fopen_s" instead of "fopen" when compiling with 
-		* Visual Studio 2015 or newer to avoid a compiler warning
-		*/
-		#if _MSC_VER >= 1900
-		if (fopen_s(&ctx.fp, bmp_file, "rb")) break;
-		#else
-		if (!(ctx.fp = fopen(bmp_file, "rb"))) break;
-		#endif
+	* Visual Studio 2015 or newer to avoid a compiler warning
+	*/
+	#if _MSC_VER >= 1900
+	if (fopen_s(&ctx.fp, bmp_file, "rb")) break;
+	#else
+	if (!(ctx.fp = fopen(bmp_file, "rb"))) break;
+	#endif
         
         if(!Validate(&ctx))                  break;
         if(!Decode(&ctx))                    break;
